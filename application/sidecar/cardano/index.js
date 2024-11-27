1942e46f0c9d4cb0b56730f5426f8b8973a2a7f5
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2024-11-27 02:30:40 +0000

    wallet_auth WIP - hardware wallet auth verification

--- application/sidecar/cardano/index.js
+++ application/sidecar/cardano/index.js
@@ -16,6 +16,44 @@ const publicKeyToStakeKey = (publicKey, networkMode) => {
     return CSL.Address.from_bytes(toHexBuffer(stakeArg));
 };
 
+const verifyTransaction = (event) => {
+    try {
+        const { transactionCbor, walletAuthChallengeHex, stakeKeyAddress } = event;
+        const resp_object = {
+            valid_transaction: false,
+            valid_signature: false,
+            valid_nonce: false,
+        }
+        const stake_key_hex = CSL.Address.from_bech32(stakeKeyAddress).to_hex().substring(2);
+        const tx = CSL.Transaction.from_bytes(toHexBuffer(transactionCbor));
+        const witnesses = tx.witness_set();
+        // const tx_hash = CSL.hash_transaction(tx.body()).to_hex();
+        const tx_hash = CSL.FixedTransaction.from_bytes(tx.body().to_bytes()).transaction_hash();
+        const tx_metadata_hash = tx.body().auxiliary_data_hash().to_hex();
+        const test_metadata_hash = CSL.hash_auxiliary_data(tx.auxiliary_data()).to_hex();
+        const nonce_entry_value = tx.auxiliary_data().metadata().get(CSL.BigNum.from_str('8'));
+        let nonce_val = '';
+        for (let i = 0; i < nonce_entry_value.as_list().len(); i++) {
+            nonce_val += nonce_entry_value.as_list().get(i).as_text();
+        }
+        resp_object.valid_nonce = (nonce_val === walletAuthChallengeHex && test_metadata_hash === tx_metadata_hash);
+        resp_object.valid_transaction = tx.is_valid();
+        for (let i = 0; i < witnesses.vkeys().len(); i++) {
+            const witness = witnesses.vkeys().get(i);
+            const WitnessVkey = witness.vkey();
+            const WitnessPubKey = WitnessVkey.public_key();
+            if (WitnessPubKey.hash().to_hex() === stake_key_hex) {
+                resp_object.valid_signature = WitnessPubKey.verify(toHexBuffer(tx_hash), witness.signature());
+            }
+        }
+        return {
+            isValid: (resp_object.valid_transaction && resp_object.valid_signature && resp_object.valid_nonce),
+        };
+    } catch (error) {
+        return { error: error.message || error };
+    }
+};
+
 const verifySignature = (event) => {
     try {
         const { signatureCbor, signatureKey, walletAuthChallengeHex, stakeKeyAddress, networkMode } = event;
@@ -35,9 +73,7 @@ const verifySignature = (event) => {
             signatureValidates,
         };
     } catch (error) {
-        return {
-            error: error.message,
-        };
+        return { error: error.message || error };
     }
 };
 
@@ -50,6 +86,7 @@ const generateNewWallet = () => {
 exports.handler = async (event) =>
 {
     switch (event.type) {
+        case 'verifyTransaction': return verifyTransaction(event);
         case 'verifySignature': return verifySignature(event);
         case 'generateNewWallet': return generateNewWallet();
     }
