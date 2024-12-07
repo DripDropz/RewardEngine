55aee94d4d6c391d984ea7c684c34a4f39a9e344
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2024-12-07 23:44:12 +0000

    qualifiers Implement logic to flag the qualified players based on meeting requirements

--- application/app/Console/Commands/FlagQualifiersHydraDoomEventsCommand.php
+++ application/app/Console/Commands/FlagQualifiersHydraDoomEventsCommand.php
@@ -2,13 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\ProjectAccountStats;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
 class FlagQualifiersHydraDoomEventsCommand extends Command
 {
-    // const QUALIFIER_DATE_START = '2024-12-03 00:00:00';
-    const QUALIFIER_DATE_START = '2024-11-01 00:00:00';
+    const QUALIFIER_DATE_START = '2024-12-03 00:00:00';
     const QUALIFIER_DATE_END = '2024-12-03 23:59:59';
     const REQUIRED_KILL_COUNT = 25;
     const REQUIRED_PLAY_MINUTES = 15;
@@ -35,26 +36,67 @@ class FlagQualifiersHydraDoomEventsCommand extends Command
         // Find all qualified players by kill count
         $qualifiedPlayersByKillCount = $this->loadQualifiedPlayersByKillCount();
 
+        // Debug
+        $this->info(sprintf(
+            'Found %d players who meet the required %d kill count',
+            count($qualifiedPlayersByKillCount),
+            self::REQUIRED_KILL_COUNT,
+        ));
+
         // For each qualified player
         foreach ($qualifiedPlayersByKillCount as $qualifiedPlayer)
         {
             // Load qualified player session events
             $projectAccountSessionEvents = $this->loadProjectAccountSessionEventsWithinQualificationPeriod($qualifiedPlayer->project_account_id);
 
-            // Check if player played for required minutes
-            if ($this->playerPlayedRequiredMinutes($projectAccountSessionEvents))
-            {
-                // Player qualified
-                dd('qualified!');
-            }
-            dd('not qualified!');
+            // Calculate total play time in minutes
+            $totalPlayTimeMinutes = $this->calculateTotalPlayTimeMinutes($projectAccountSessionEvents);
+
+            // Update project account stats
+            $playedDuringQualifierPeriod = count($projectAccountSessionEvents) > 0;
+            $achievedRequiredKillCount = ((int) $qualifiedPlayer->total_kills >= self::REQUIRED_KILL_COUNT);
+            $achievedRequiredPlayMinutes = ($totalPlayTimeMinutes >= self::REQUIRED_PLAY_MINUTES);
+            ProjectAccountStats::query()
+                ->where('project_id', $qualifiedPlayer->project_id)
+                ->where('project_account_id', $qualifiedPlayer->project_account_id)
+                ->update([
+                    'qualifier' => [
+                        'is_qualified' => ($playedDuringQualifierPeriod && $achievedRequiredKillCount && $achievedRequiredPlayMinutes),
+                        'requirements' => [
+                            [
+                                'play_from' => self::QUALIFIER_DATE_START,
+                                'play_to' => self::QUALIFIER_DATE_END,
+                                'is_met' => $playedDuringQualifierPeriod,
+                            ],
+                            [
+                                'required_kill_count' => self::REQUIRED_KILL_COUNT,
+                                'actual_kill_count' => (int) $qualifiedPlayer->total_kills,
+                                'is_met' => $achievedRequiredKillCount,
+                            ],
+                            [
+                                'required_play_minutes' => self::REQUIRED_PLAY_MINUTES,
+                                'actual_play_minutes' => $totalPlayTimeMinutes,
+                                'is_met' => $achievedRequiredPlayMinutes,
+                            ],
+                        ],
+                    ],
+                ]);
+
+            // Debug
+            $this->info(sprintf(
+                'Done processing project account id: %d',
+                $qualifiedPlayer->project_account_id,
+            ));
         }
+
+        // Debug
+        $this->info('Task Completed');
     }
 
     private function loadQualifiedPlayersByKillCount(): array
     {
         $sql = <<<QUERY
-select project_account_id
+select project_id, project_account_id, JSON_EXTRACT(stats, '$.qualifier.kill') as `total_kills`
 from project_account_stats
 where CAST(JSON_EXTRACT(stats, '$.qualifier.kill') AS UNSIGNED) >= ?
 QUERY;
@@ -66,15 +108,14 @@ QUERY;
     {
         $sql = <<<QUERY
 select
-    project_account_session_events.event_type,
-    project_account_session_events.event_timestamp,
-    project_account_session_events.game_id
+    max(project_account_session_events.game_id) as game_id,
+    min(project_account_session_events.event_timestamp) as earliest_event_timestamp,
+    max(project_account_session_events.event_timestamp) as latest_event_timestamp
 from project_account_sessions
 join project_account_session_events on project_account_session_events.reference = project_account_sessions.reference
 where project_account_sessions.project_account_id = ?
-  and project_account_session_events.event_type IN ('game_started', 'game_finished')
   and (project_account_session_events.event_timestamp >= ? AND project_account_session_events.event_timestamp <= ?)
-order by event_timestamp asc;
+group by project_account_session_events.game_id
 QUERY;
 
         return DB::select($sql, [
@@ -84,35 +125,18 @@ QUERY;
         ]);
     }
 
-    private function playerPlayedRequiredMinutes(array $projectAccountSessionEvents): bool
+    private function calculateTotalPlayTimeMinutes(array $projectAccountSessionEvents): float
     {
-        $groupedProjectAccountSessionEvents = [];
+        $totalPlayTimeMinutes = 0;
 
         foreach ($projectAccountSessionEvents as $projectAccountSessionEvent) {
-            if (!isset($groupedProjectAccountSessionEvents[$projectAccountSessionEvent->game_id])) {
-                $groupedProjectAccountSessionEvents[$projectAccountSessionEvent->game_id] = [
-                    'game_started' => 0,
-                    'game_finished' => 0,
-                ];
-            }
-            if ($projectAccountSessionEvent->event_type === 'game_started') {
-                $groupedProjectAccountSessionEvents[$projectAccountSessionEvent->game_id]['game_started'] = (int) $projectAccountSessionEvent->event_timestamp;
-            }
-            if ($projectAccountSessionEvent->event_type === 'game_finished') {
-                $groupedProjectAccountSessionEvents[$projectAccountSessionEvent->game_id]['game_finished'] = (int) $projectAccountSessionEvent->event_timestamp;
+            if (!empty($projectAccountSessionEvent->earliest_event_timestamp) && !empty($projectAccountSessionEvent->latest_event_timestamp)) {
+                $earliestEventTimestamp = Carbon::createFromTimestampMs($projectAccountSessionEvent->earliest_event_timestamp);
+                $latestEventTimestamp = Carbon::createFromTimestampMs($projectAccountSessionEvent->latest_event_timestamp);
+                $totalPlayTimeMinutes += round($earliestEventTimestamp->diffInMinutes($latestEventTimestamp), 2);
             }
         }
 
-        $target = (60 * self::REQUIRED_PLAY_MINUTES);
-        foreach ($groupedProjectAccountSessionEvents as $groupedProjectAccountSessionEvent) {
-            $gameStarted = round($groupedProjectAccountSessionEvent['game_started'] / 1000);
-            $gameFinished = round($groupedProjectAccountSessionEvent['game_finished'] / 1000);
-            $delta = (int) ($gameFinished - $gameStarted);
-            if ($delta >= $target) {
-                return true;
-            }
-        }
-
-        return false;
+        return $totalPlayTimeMinutes;
     }
 }
