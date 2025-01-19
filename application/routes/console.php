cfd60333ea180f93e03048c772c83f3f000197fa
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-01-19 02:28:15 +0000

    main Disable crons

--- application/routes/console.php
+++ application/routes/console.php
@@ -5,7 +5,7 @@ use App\Console\Commands\SyncHydraDoomGlobalStatsCommand;
 use Illuminate\Support\Facades\Schedule;
 
 // Sync hydra doom global stats every minute
-Schedule::command(SyncHydraDoomGlobalStatsCommand::class)->everyMinute();
+// Schedule::command(SyncHydraDoomGlobalStatsCommand::class)->everyMinute();
 
 // Generate hydra doom leaderboard every five minutes
-Schedule::command(GenerateHydraDoomLeaderboardCommand::class)->everyFiveMinutes();
+// Schedule::command(GenerateHydraDoomLeaderboardCommand::class)->everyFiveMinutes();
